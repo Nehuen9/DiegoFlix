0615b72d0296487108a2451714e6972842835d91
Diego Nehuen <nehuenvd@gmail.com>
2025-01-24 22:37:34 +0100

    OR, AND, NOT y tambien IN , BETWEEN

--- MaxiFlix.sql
+++ MaxiFlix.sql
@@ -27,3 +27,53 @@ Where id = 43
 Select * From Paises 
 Where id = 39
 
+-------------24/1/25---------------------------------------
+--OR  
+-- AND
+--NOT
+
+Select * From Peliculas
+Where Id = 9
+
+
+Select * From [Peliculas.Reparto] Where IdPelicula = 9
+
+
+Select * From Reparto
+Where Id = 5 OR Id = 10 Or Id = 15
+
+
+Select * From Peliculas
+Where NOT (MinutosDuracion >=100 AND  MinutosDuracion <= 120)
+AND Bio LIKE '%POLIC%'
+OR Bio LIKE '%amor%'
+OR Bio LIKE '%cien%'
+OR Bio LIKE '%los%'
+
+
+
+
+----------------------------
+
+-- IN 
+-- BETWEEN
+--funciones integradas
+-- count , max , min, 
+
+Select * From Peliculas
+
+Select * From [Peliculas.Reparto] Where IdPelicula = 12
+
+Select * From Reparto
+Where Id IN (10, 15, 66, 67)
+
+
+Select * From Reparto
+Where Id BETWEEN 62 AND 67
+
+
+Select * From Peliculas 
+Where year(FechaEstreno) BETWEEN 2000 AND 2010
+
+
+
