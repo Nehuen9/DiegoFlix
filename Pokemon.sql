8babe6e1c3cd917a52ed39a756b255c5fcea5554
Diego Nehuen <nehuenvd@gmail.com>
2025-07-06 23:11:42 +0200

    diagrama nuevo

--- Pokemon.sql
+++ Pokemon.sql
@@ -129,9 +129,97 @@ ALTER TABLE [Pokemons.Habilidades]
 ADD CONSTRAINT FK_PokeHabilidad_Pokemons FOREIGN KEY (IdPokemon) REFERENCES Pokemons(Id)
 
 
+--ALTER MODIFICAR COLUMNAS!!!!
 
 
 
+
+
+-----------------------------------------------------------------
+--Use Master;
+--go
+--Create database CatalogoWebDB
+-- MDF : MASTER DE DATOS master data file
+
+-- LDF: log data file SE GUARDA LAS COSAS QUE HACEMOS CON LOS DATOS(RELACIONES, ELIMINAR, CAMBIAR , MODIFICAR)
+--go
+
+
+-- creacion de tabla usuarios, en catalogo web!!!
+
+Create table USUARIO (
+Id int not null,
+Mail varchar(100),
+Pass Varchar(100),
+Nombre Varchar (100), 
+Apellido Varchar(100),
+UrlImagenPerfil Varchar(100),
+Admin Varchar (100)
+
+);
+
+--Creacion de tabla articulos en catalogo webªª!!!
+
+create table ARTICULOS (
+Id int not null,
+Codigo Varchar(50),
+Nombre varchar(50),
+Descripcion varchar (300), 
+IdMarca int, 
+IdCategoria int, 
+ImagenUrl Varchar(100),
+Precio Varchar(10)
+--verificar que dato se pone en precio
+
+);
+
+
+
+--Creacion de tabla MARCAS
+
+create table MARCAS (
+Id int not null, 
+Descripcion Varchar (300), 
+
+);
+
+
+
+--creacion de tabla CATEGORIAS.
+
+create table CATEGORIAS (
+Id int not null, 
+Descripcion Varchar (300),
+);
+
+
+--Creacion de taba de FAVORITOS
+
+create table FAVORITOS (
+Id int not null,
+IdUsuario int not null, 
+IdArticulo int not null,
+);
+
+
+ALTER TABLE usuario 
+ADD CONSTRAINT PK_Usuarios PRIMARY KEY (Id)
+
+
+ALTER TABLE ARTICULOS 
+ADD CONSTRAINT PK_Articulos PRIMARY KEY (Id)
+--ADD CONSTRAINT FK_
+
+
+
+ALTER TABLE MARCAS 
+ADD CONSTRAINT PK_MARCAS PRIMARY KEY (ID)
+
+ALTER TABLE CATEGORIAS 
+ADD CONSTRAINT PK_CATEGORIAS PRIMARY KEY (ID)
+
+ALTER TABLE ARTICULOS
+ADD CONSTRAINT FK_MARCAS FOREIGN KEY (IdMarcas) REFERENCES MARCAS (ID)
 
 
 
